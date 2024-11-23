05dce59dce5142783433200d4e6484695565e827
Taufika24 <jtaufika243@gmail.com>
2024-11-23 20:08:53 +0530

    Create cube.c

---
+++ cube.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    float a;
+    printf("enter a value");
+    scanf("%f",&a);
+    printf("The cube of the number is %f" ,a*a*a);
+    return 0;
+}
